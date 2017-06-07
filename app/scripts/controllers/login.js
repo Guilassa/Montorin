d83c8bbc798150d015e77453e3c9a7169655bbf9
Guillermo <guilassa0809@gmail.com>
2017-06-07 12:08:55 +0200

    Introduccion a la herramienta Yeoman

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name holaApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the holaApp
+ */
+angular.module('holaApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- connectdb.php
+++
@@ -1,9 +0,0 @@
-<?php 
-define("HOSTNAME","localhost");
-define("USERNAME","root");
-define("PASSWORD","root");
-define("DATABASE","mhtour");
-
-$dbhandle=new mysqli(HOSTNAME,USERNAME,PASSWORD,DATABASE) or die("Unable to Connect DB");
-
-?>
