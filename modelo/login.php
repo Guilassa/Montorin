5d907b6c55f629855abfde5fe9eb5a80d751f0da
Alvaro <alroastorga@gmail.com>
2017-06-07 12:36:44 +0200

    Estructura final angular y yeoman

--- modelo/login.php
+++ modelo/login.php
@@ -21,7 +21,7 @@
 							$usuario_login = $_POST['login'];     //recibidos de formulario
 							$password_login = $_POST['password'];
 							
-						    //Query para seleccionar el usuario introducido por login
+								//Query para seleccionar el usuario introducido por login
 							$sql = "SELECT * FROM hoja1 WHERE Nombre = '$usuario_login'";   // si usuario de base es = a usuario recogido de login
 							
 							//Recogemos los datos de la query en una variable
@@ -31,25 +31,7 @@
 							if ($result->num_rows > 0) {
 							    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 								$password = $row['Password'];  //recogremos pass de DB
-							    $categoria = $row['Categoria']; //recogenos ctegoria de DB
-								
-								
-								$apellido1 = $row['Apellido1'];  
-								$apellido2 = $row['Apellido2'];  
-								$dni = $row['Dni']; 
-								$fecha_alta = $row['Fecha_alta']; 
-								$nombre_empresa = $row['Nombre_empresa']; 
-								$cif_empresa = $row['Cif_empresa']; 
-								$iban = $row['IBAN']; 
-								$prorrata_extra = $row['Prorrata_extra']; 
-								$fecha_baja_laboral = $row['Fecha_baja_laboral'];
-								$fecha_alta_laboral = $row['Fecha_alta_laboral'];
-								$horas_extra_forzadas = $row['Horas_extra_forzadas'];
-								$horas_extra_voluntarias = $row['Horas_extra_voluntarias'];
-								$email = $row['email']; 
-								
-								
-								
+							    $tipoUser = $row['Categoria']; //recogenos ctegoria de DB
 								
 							}
 							
@@ -59,39 +41,17 @@
 								
 								$_SESSION['loggedin'] = true;
 							    $_SESSION['usuario'] = $usuario_login;
-							  
-								$_SESSION['apellido1'] = $apellido1;
-								$_SESSION['apellido2'] = $apellido2;
-								$_SESSION['dni'] = $dni;
-								$_SESSION['fecha_alta'] = $fecha_alta;
-								$_SESSION['categoria'] = $categoria;
-								$_SESSION['nombre_empresa'] = $nombre_empresa;
-								$_SESSION['cif_empresa'] = $cif_empresa;
-								$_SESSION['iban'] = $iban;
-								$_SESSION['prorrata_extra'] = $prorrata_extra;
-								$_SESSION['fecha_baja_laboral'] = $fecha_baja_laboral;
-								$_SESSION['fecha_alta_laboral'] = $fecha_alta_laboral;
-								$_SESSION['horas_extra_forzadas'] = $horas_extra_forzadas;
-								$_SESSION['horas_extra_voluntarias'] = $horas_extra_voluntarias;
-								$_SESSION['email'] = $email;
-								
-								
-								
-								
-								
-								
-								
-								
+							    $_SESSION['tipo'] = $tipoUser;
 							    $_SESSION['start'] = time();
 							    $_SESSION['expire'] = $_SESSION['start'] + (5 * 60);    // sino esta activo  surante este tiempo desloga
 								 
 								  
-								  if (strcmp($categoria, "Administrativo")==0){   // comprobamos si el tipo de usuario es admin   sino  else
+								  if (strcmp($tipoUser, "Administrativo")==0){   // comprobamos si el tipo de usuario es admin   sino  else
 								    $_SESSION["usuario"]=$_POST["login"];//alamcenmos en usuario persona que se ha logado
 							    	header("location:../vista/paginaAdministrador.php");
 							       } else {
 									$_SESSION["usuario"]=$_POST["login"];//alamcenmos en usuario persona que se ha logado
-									header("location:../vista/paginaOtrosEmpleados2.php");
+									header("location:../vista/paginaOtrosEmpleados.php");
 							       }
 								
 								
--- vista/paginaOtrosEmpleados.php
+++ vista/paginaOtrosEmpleados.php
@@ -1,22 +1,13 @@
 <html>
 <head>
-<meta charset="utf-8">
+
+ <meta charset="utf-8">
 <title></title>
 <link rel="stylesheet" type="text/css" href="../styles/paginaOtrosEmpleados.css" media="screen" />
-
-
-
-        <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400" rel="stylesheet" />
-		<link rel="stylesheet" href="../styles/cssMenu/fontello.css" />
-		<link rel="stylesheet" href="../styles/cssMenu/normalize.css" />
-		
-		<link rel="stylesheet" href="../styles/cssMenu/index.css" />
-		
-		<link rel="stylesheet" href="../styles/cssMenu/sidebar.css" />
-
-
 </head>
 <body>
+
+
 <a href="../styles/cierreDeSesion.php">Cierra Sesión</a>
 
 
@@ -33,95 +24,28 @@
     ?>
 <?php           // en otro caso osea si alguien ha inciado sesion
 	
+	echo "<h2> Pagina otros empleados</h2>";
 
-    //recogemos datos de sesion
-    $nombre= $_SESSION["usuario"];
-	$apellido1=$_SESSION['apellido1'];
-	$apellido2=$_SESSION['apellido2'];
-	$categoria=$_SESSION['categoria'];
-	$email=$_SESSION['email'];
-	$empresa=$_SESSION['nombre_empresa'];
-							
+    echo "BienVenido: " .$_SESSION["usuario"]."<br><br>";
+
     ?>
     
 
 
-
-
-
-
- 
- <div class="wrapper jsc-sidebar-content jsc-sidebar-pulled">
-	<nav>
-		<a href="#" class="icon-menu link-menu jsc-sidebar-trigger"></a>
-	</nav>
-	<section class="main-content">
-    
-    
-		<div class="row">
-<div class="column">
-  <div class="card"> <img src="../imagenes/team1.jpg" alt="Jane" style="width:100%">
-    <div class="container">
-      <h2><?php echo $nombre ?></h2>                <!--imprimimos con querys-->
-      <p class="title"><?php echo $categoria ?></p>
-      <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-      <p><?php echo $email ?></p>
-      <p><?php echo $empresa ?></p>
-      <p>
-        <button class="button">Contact</button>
-      </p>
+ <div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="../imagenes/team1.jpg" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Jane Doe</h2>
+        <p class="title">CEO &amp; Founder</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
     </div>
   </div>
 </div>
-
-	</section>
-    
-</div>
-
-		<nav class="sidebar jsc-sidebar" id="jsi-nav" data-sidebar-options="">
-	<ul class="sidebar-list">
-		<li><a href="paginaOtrosEmpleados.php" class="current">SidebarJS</a></li>
-		<li><a href="http://github.com/makotot/sidebar/">View on Github</a></li>
-		<li><a href="http://github.com/makotot/sidebar/releases">Download</a></li>
-	</ul>
-</nav>
-
-
-		<script src="../styles/cssMenu/jquery.min.js"></script>
-		
-		<script src="../styles/cssMenu/sidebar.js"></script>
-		
-		<script>
-			$('#jsi-nav').sidebar({
-				trigger: '.jsc-sidebar-trigger',
-				pullCb: function () { console.log('pull'); },
-				pushCb: function () { console.log('push'); }
-			});
-		</script>
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-
-    
-    
-
-    
-  
-    
 
 
 
